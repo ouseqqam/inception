d669226c37db0e36fb40c2085bc3fd91a9fd3373
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-10-20 19:21:16 +0100

    config between wp and mariadb work

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-service mysql start 
+
+service mysql start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -u root -e "CREATE USER 'wp'@'%' IDENTIFIED BY '1234';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wp'@'%' IDENTIFIED BY '1234';"
+mydsql -u root -e "FLUSH PRIVILEGES;"
 mysqld_safe
 tail -f > /dev/null
--- wordpress/script.sh
+++ wordpress/script.sh
@@ -4,14 +4,13 @@ apt-get install -y wget  php-cgi php-common php-fpm php-pear php-mbstring php-zi
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 rm -rf latest.tar.gz
-mv wordpress/wp-config-sample.php wordpress/wp-config.php
 chown -R www-data:www-data wordpress
+mv wordpress/wp-config-sample.php wordpress/wp-config.php
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-sed -i 's/database_name_here/inception/g' wordpress/wp-config.php
+sed -i 's/database_name_here/wordpress/g' wordpress/wp-config.php
 sed -i 's/username_here/wp/g' wordpress/wp-config.php
 sed -i 's/password_here/1234/g' wordpress/wp-config.php
-sed -i 's/localhost/mariadb:3306/g' wordpress/wp-config.php
-
+sed -i 's/localhost/mariadb/g' wordpress/wp-config.php
 
 service php7.3-fpm start
 tail -f > /dev/null
